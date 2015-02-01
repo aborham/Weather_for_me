da41a532478003bd46966406500edd5ef6b33256
=Ahmed Borham <=saladin2002eg@gmail.com>
2015-02-02 01:27:13 +0200

    adding refresh menu button

--- app/src/main/java/com/borham/ahmed/weather/MainActivity.java
+++ app/src/main/java/com/borham/ahmed/weather/MainActivity.java
@@ -1,18 +1,26 @@
 package com.borham.ahmed.weather;
 
 import android.support.v7.app.ActionBarActivity;
-//import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
+import java.io.IOException;
+import java.net.MalformedURLException;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-//import android.os.Build;
+import android.os.Build;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -26,7 +34,7 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new ForecastFragment())
                     .commit();
         }
     }
@@ -54,49 +62,5 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
 
-        private ArrayAdapter<String> mForecastAdaptar;
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
-
-            String[] forecastArray={
-                    "Today-80/60",
-                    "Tomorrow - 56/55",
-                    "Tomorrow - 56/55",
-                    "Tomorrow - 56/55",
-                    "Tomorrow - 56/55",
-                    "Tomorrow - 56/55",
-                    "Tomorrow - 56/55",
-                    "Tomorrow - 56/55",
-                    "Tomorrow - 56/55",
-                    "Wed - 44/55"
-            };
-            List<String> weekForecast = new ArrayList<>(
-                    Arrays.asList(forecastArray)
-            );
-
-            mForecastAdaptar = new ArrayAdapter<>(
-                    getActivity(),
-                    R.layout.list_item_forecast,
-                    R.id.list_item_forecast_textview,
-                    weekForecast
-            );
-            ListView listView = (ListView) rootView.findViewById(
-                    R.id.listview_forecast
-            );
-            listView.setAdapter(mForecastAdaptar);
-
-            return rootView;
-        }
-    }
 }
